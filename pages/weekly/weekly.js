9fa71e10b0f560cf66117c7bd36661f88fc5bd34
wwwjf <wwwjf2015@outlook.com>
2019-12-03 01:04:33 +0800

    request url

--- pages/weekly/weekly.js
+++ pages/weekly/weekly.js
@@ -12,31 +12,56 @@ Page({
     },
     weeklyMovieList: [
       {
+        id:77,
         name: "《泰坦尼克号》",
         comment: "失去的才是永恒的",
         imagePath: "/images/titanic.jpg",
         isHighlyRecommended: false,
       },
       {
+        id: 88,
         name: "《这个杀手不太冷》",
         comment: "小萝莉与怪蜀黍纯真灿烂的爱情故事",
         imagePath: "/images/leon.jpg",
         isHighlyRecommended: false,
       },
       {
+        id: 99,
         name: "《教父》",
         comment: "最精彩的剧本，最真实的黑帮电影。",
         imagePath: "/images/jf.jpg",
         isHighlyRecommended: true,
       }
-    ]
+    ],
+    count:0,
+    currentIndex:0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.setData({
+      currentIndex:this.data.weeklyMovieList.length-1,
+    })
+  },
+
+  f0:function(event){
+    console.log("==="+this.data.count)
+    // this.data.count = this.data.count+1
+    this.setData({
+      count:this.data.count+1,
+      "weeklyMovieList[2].name":"《教父3》",
+      currentIndex:this.data.weeklyMovieList.length-1,
+    })
+  },
+
+  f1:function(event){
+    var mid = event.currentTarget.dataset.movieId
+    console.log("----movieId:"+mid)
+    wx.navigateTo({
+      url: '/pages/detail/detail?id='+mid
+      })
   },
 
   /**
